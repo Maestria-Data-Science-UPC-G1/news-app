753190b63a6d53f5d9436aa861b34b2d80fea773
Ernesto Igor Laura Mamani <igorov87@gmail.com>
2023-10-17 16:43:26 -0500

    Content-Security-Policy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
