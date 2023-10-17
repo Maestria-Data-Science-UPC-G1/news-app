07e26278bef2acc13f7270ec1da1bf20629fa48f
Ernesto Igor Laura Mamani <igorov87@gmail.com>
2023-10-17 12:56:22 -0500

    changes

--- components/News.tsx
+++ components/News.tsx
@@ -90,7 +90,7 @@ const News = () => {
                     </thead>
                     <tbody>
                         {news.map((node) => (
-                            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                            <tr key={node.id} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                                 <th scope="row" className="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {node.id}
                                 </th>
@@ -111,24 +111,6 @@ const News = () => {
                                 </td>
                             </tr>
                         ))}
-
-                        <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="px-6 py-4">
-                                Silver
-                            </td>
-                            <td className="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
                     </tbody>
                 </table>
             </div>
