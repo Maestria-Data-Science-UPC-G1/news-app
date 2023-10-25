8165de54c68c9d67a4c1f9ee91da1ccca9d80f1a
Ernesto Laura <igorov87@gmail.com>
2023-10-25 15:04:16 -0500

    alto de zona de grafo

--- components/News.tsx
+++ components/News.tsx
@@ -71,7 +71,7 @@ const News = () => {
 
             {loadingGraph ?
                 <Spinner /> :
-                <NetworkDiagram data={graph} width={1000} height={600} />
+                <NetworkDiagram data={graph} width={1000} height={500} />
             }
 
             <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
